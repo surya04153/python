96d010dcb28e7f7014c0c8e727e192f52d860a5e
suryapanda <suryanarayan.panda@gmail.com>
2024-09-10 23:38:45 -0700

    MyFirstPythonOnGit

---
+++ Main.py
@@ -0,0 +1,18 @@
+print("Hello, World!")
+# Input: two numbers
+#num1 = float(input("Enter first number: "))
+#num2 = float(input("Enter second number: "))
+
+# Add the two numbers
+#sum1 = num1 + num2
+
+# Output the result
+#print("The sum of", num1, "and", num2, "is:", sum1)
+
+age= int(input("Enter your age:"))
+
+if age>18 :
+    print("sabalaka")
+else:
+    print("nabalaka")
+
